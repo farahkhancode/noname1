ae3056d8ed820c91795d501bbbbcb39116602817
farahmed@gmail.com <farahmed@gmail.com>
2018-10-18 10:05:19 -0500

    location in progress

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -11,8 +11,10 @@ module.exports = {
     const hashedPassword = bcrypt.hashSync(newUser.password, salt);
 
 // #4
+   console.log(newUser);
     return User.create({
       email: newUser.email,
+      locationId:newUser.locationId,
       password: hashedPassword
     })
     .then((user) => {
@@ -21,7 +23,8 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
-  },
+
+  }
 
 
 
